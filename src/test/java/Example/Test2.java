adbbdacf4354cd81cc45577ed61a0a09b7c2e994
nvupkoor <nvupkoor@cisco.com>
2016-08-16 07:44:32 +0530

    new test

---
+++ src/test/java/Example/Test2.java
@@ -0,0 +1,19 @@
+package Example;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class Test2 {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
